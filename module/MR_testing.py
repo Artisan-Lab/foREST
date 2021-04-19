67454bee9338180ec2f70291910e7749717b8b97
linjiaxian <ljx425115084@gmail.com>
2021-04-19 22:44:05 +0800

    commit

--- module/MR_testing.py
+++ module/MR_testing.py
@@ -2,79 +2,77 @@ import requests
 import os.path
 from prance import ResolvingParser
 import json
-import random
-from random import randrange
 import parse
+import random
 from datetime import timedelta, datetime
-
-
-def add_token(url):
-    if '?' in url:
-        url = url + '&private_token=n19y6WJgSgjyDuFSHMx9'
-        # ehWyDYxYMRcFLKCRryeK root token
-    else:
-        url = url + '?private_token=n19y6WJgSgjyDuFSHMx9'
-    return url
-
-
-def random_str(slen=10):
-    seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-"
-    sa = []
-    for i in range(slen):
-      sa.append(random.choice(seed))
-    return ''.join(sa)
+from random import randrange
 
 
 def random_date(start, end):
+    # 随机生成start与end之间的日期 format：ISO 8601
     delta = end - start
     int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
     random_second = randrange(int_delta)
     return start + timedelta(seconds=random_second)
 
 
-def fuzz_paramerter(field_info):
+def random_str(slen=10):
+    # 随机生成字符串，默认长度为10
+    seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-"
+    sa = []
+    for i in range(slen):
+        sa.append(random.choice(seed))
+    return ''.join(sa)
+
+
+def fuzz_parameter(field_info):
     if field_info.enum:
         return field_info.field_name + '=' + str(random.choice(field_info.enum))
     elif field_info.format:
         if field_info.format == 'ISO 8601 YYYY-MM-DDTHH:MM:SSZ':
             return field_info.field_name + '=' + \
-                   str(random_date(datetime(2019,1,1,0,0,0).astimezone().replace(microsecond=0),
-                    datetime(2021,12,31,23,59,59).astimezone().replace(microsecond=0)).isoformat())
+                   str(random_date(
+                       datetime(2021, 12, 31, 23, 59, 59).astimezone().replace(microsecond=0)).isoformat())
     else:
         if field_info.field_type == 'boolean':
             return field_info.field_name + '=' + random.choice(['true', 'false'])
         elif field_info.field_type == 'string':
-            return field_info.field_name + '=' + random_str(random.randint(0,10))
+            return field_info.field_name + '=' + random_str()
         elif field_info.field_type == 'integer':
-            return field_info.field_name + '=' + str(random.randint(0,50))
+            return field_info.field_name + '=' + str(random.randint(0, 50))
     pass
 
 
-def get_responses(url, req_paramerter, responses_list, default_count):
-    fuzz_parameter = fuzz_paramerter(req_paramerter)
-    url1 = url + '&' + fuzz_parameter
-    request_respones = requests.get(url1)
-    respones_status = request_respones.status_code
-    if 300 > respones_status >= 200 and default_count < 10:
-        if json.loads(request_respones.text) == []:
-            responses_list = get_responses(url, req_paramerter, responses_list, default_count + 1)
+def add_token(url):
+    # 加上token 因为暂时不考虑token的问题 所以在第一步就加上token
+    # if '?' in url:
+    #     url = url + '&private_token=n19y6WJgSgjyDuFSHMx9'
+    #     # ehWyDYxYMRcFLKCRryeK root token
+    # else:
+    url = url + '?private_token=n19y6WJgSgjyDuFSHMx9'
+    return url
+
+
+def get_responses(url, req_parameter, responses_list, default_count):
+    # 获取一个响应列表 其中 列表第一项为源输出（不加参数的输出）
+    parameter = fuzz_parameter(req_parameter)
+    url1 = url + '&' + parameter
+    request_response = requests.get(url1)
+    response_status = request_response.status_code
+    if 300 > response_status >= 200 and default_count < 10:
+        if not json.loads(request_response.text):
+            responses_list = get_responses(url, req_parameter, responses_list, default_count + 1)
         else:
             responses_list.append(json.loads(requests.get(url1).text))
-    elif 500 > respones_status >= 300 and default_count < 10:
-        responses_list = get_responses(url, req_paramerter, responses_list, default_count + 1)
-    elif 500 == respones_status:
+    elif 500 > response_status >= 300 and default_count < 10:
+        responses_list = get_responses(url, req_parameter, responses_list, default_count + 1)
+    elif 500 == response_status:
         print('find bug in %s' % url1)
     return responses_list
 
 
-def MR_texting(responses_list, MR_matric):
-    #subset  equality equivalence disjoint complete diffirence
-    response_text = responses_list[0]
-    response_text1 = random.choice(responses_list[1:11])
-    response_text2 = random.choice(responses_list[1:11])
-    for i in range(10):
-        if response_text1 == response_text2:
-            response_text2 = random.choice(responses_list[1:11])
+def MR_texting(response_text, response_text1, response_text2, MR_matric):
+    # MR_matric 的 含义分别为 subset  equality equivalence disjoint complete diffirence
     response_text3 = response_text1 + response_text2
     if (all([response_text[i] in response_text1 for i in range(0,len(response_text))]) or \
             all([response_text1[i] in response_text for i in range(0, len(response_text1))])):
@@ -90,13 +88,15 @@ def MR_texting(responses_list, MR_matric):
             all([response_text1[i] not in response_text2 for i in range(0,len(response_text1))])):
         MR_matric[3] = MR_matric[3] + 1
     if (all([response_text[i] in response_text3 for i in range(0,len(response_text))]) and
-            all([response_text3[i] in response_text for i in range(0,len(response_text3))])):
+            all([response_text3[i] in response_text for i in range(0,len(response_text3))])) and \
+            response_text1 != response_text2:
         MR_matric[4] = MR_matric[4] + 1
     return MR_matric
 
 
 def metamorphic(api_info,parameter_list):
     if api_info.http_method == 'get':
+        MR_dic = {}
         url = add_token(api_info.path)
         if api_info.req_field_names:
             for req_paramerter in api_info.req_param:
@@ -125,10 +125,16 @@ def metamorphic(api_info,parameter_list):
             for i in range(1,11):
                 responses = get_responses(url, req_paramerter, responses, 0)
             if len(responses) < 10:
-                print('no enough %s' % req_paramerter.field_name)
+                print('lack %s' % req_paramerter.field_name)
                 continue
             for i in range(1, 11):
-                MR_matric_count = MR_texting(responses, MR_matric_count)
+                response_text = responses[0]
+                response_text1 = random.choice(responses[1:11])
+                response_text2 = random.choice(responses[1:11])
+                for i in range(10):
+                    if response_text1 == response_text2:
+                        response_text2 = random.choice(responses[1:11])
+                MR_matric_count = MR_texting(response_text, response_text1, response_text2, MR_matric_count)
             if MR_matric_count[0] == max(MR_matric_count): #and MR_matric_count[1] + MR_matric_count[2] <MR_matric_count[0]:
                 MR_matric[0] = 1
             if MR_matric_count[1] == max(MR_matric_count):
@@ -137,7 +143,10 @@ def metamorphic(api_info,parameter_list):
                 MR_matric[2] = 1
             if MR_matric_count[3] == max(MR_matric_count):
                 MR_matric[3] = 1
-            print(req_paramerter.field_name + '   ' + str(MR_matric))
+            if MR_matric_count[4] == 10 and MR_matric_count[2] != 1:
+                MR_matric[4] = 1
+            MR_dic[str(req_paramerter.field_name)] = MR_matric
+        print(str(MR_dic))
 
 
 path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../openapi/projects-api.yaml")
