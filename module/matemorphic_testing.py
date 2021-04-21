e81f2cac8d30c10ce34066d25b9a1407b5020361
linjiaxian <ljx425115084@gmail.com>
2021-04-21 09:46:45 +0800

    commit

--- module/matemorphic_testing.py
+++ module/matemorphic_testing.py
@@ -1,3 +1,4 @@
+import requests
 from MR_testing import fuzz_parameter
 
 
@@ -9,15 +10,22 @@ class MetamorphicJudge:
         self.api_info = api_info
         self.mr_dic = mr_dic
 
-
     def sub_sub_test(self,parameter_1, parameter_2):
         parameter_1 = fuzz_parameter(parameter_1)
+        parameter_2 = fuzz_parameter(parameter_2)
+        url_1 = self.base_url + '&' + parameter_1
+        url_1_response = requests.get(url_1)
+        url_test = self.base_url + '&' + parameter_1 + '&' +
+        responses_1 =
 
     def metamorphic_testing(self):
         for parameter_1 in self.mr_dic:
             for parameter_2 in self.mr_dic:
                 if parameter_1[0] == 1 and parameter_2[0] == 1:
                     MetamorphicJudge.sub_sub_test(parameter_1,parameter_2)
+
+
+
 
 
 
