a7d7490265f14f78f05575f665a1f173cbf78567
Chen Yang <990776253@qq.com>
2021-07-09 21:06:25 +0800

    update

--- fuzz_from_data/commons/mutateConstants.py
+++ fuzz_from_data/commons/mutateConstants.py
@@ -1,6 +1,6 @@
 STRINGS_FOR_MUTATED = ['<sql>', '<script>', '</>', '<html>', 'or', '\/', '<php', '</>', '</script>', b'0x\ussssxxx']
 INTEGERS_FOR_MUTATED = [2 ** 63 - 1, -2 ** 63 - 1]
-with open('coverageData/mutateConstants.txt', 'r') as f:
+with open('commons/mutateConstants.txt', 'r') as f:
     lines = f.readlines()
     for l in lines:
         STRINGS_FOR_MUTATED.append(l)
