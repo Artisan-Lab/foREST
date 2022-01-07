15973b77efff14222aa5910ce1dc0c0d47cf3e8b
linjiaxian <ljx425115084@gmail.com>
2022-01-08 01:29:51 +0800

    改图片

--- experiment/code/draw.py
+++ experiment/code/draw.py
@@ -37,16 +37,22 @@ y2 = list()
 x3 = list()
 y3 = list()
 for i in range(0, forest_length):  # 从第二行开始读取
+    if '1:00:' in forestData[i][0]:
+        break
     date1 = datetime.strptime(forestData[i][0], '%H:%M:%S.%f')
     x1.append(date1)  # 将第一列数据从第二行读取到最后一行赋给列表x
     y1.append(int(forestData[i][2]))
 
 for _ in range(0, evomaster_length):
+    if '1:00:' in evomaster_data[_][0]:
+        break
     date2 = datetime.strptime(evomaster_data[_][0], '%H:%M:%S.%f')
     x2.append(date2)
     y2.append(int(evomaster_data[_][2]))
 
 for _ in range(0, restler_length):
+    if '1:00:' in restler_data[_][0]:
+        break
     date3 = datetime.strptime(restler_data[_][0], '%H:%M:%S.%f')
     x3.append(date3)
     y3.append(int(restler_data[_][2]))
@@ -54,9 +60,9 @@ for _ in range(0, restler_length):
 fig, ax = plt.subplots()
 
 # Plot the date using plot_date rather than plot
-ax.plot(x1, y1, 'r-', label='foREST', linewidth=1)
-ax.plot(x2, y2, 'b--', label='EvoMaster', linewidth=1)
-ax.plot(x3, y3, color='violet', linestyle='-.', label='RESTler', linewidth=1)
+ax.plot(x1, y1, 'r-', label='foREST', linewidth=3)
+ax.plot(x2, y2, 'b--', label='EvoMaster', linewidth=3)
+ax.plot(x3, y3, color='violet', linestyle='-.', label='RESTler', linewidth=3)
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 plt.ylabel('Code coverage')
@@ -72,7 +78,7 @@ ax.yaxis.set_major_locator(y_major_locator)
 # plt.ylim((10000, 30000))
 plt.title('GitLab-projects')
 plt.legend(bbox_to_anchor=(1, 0.11), loc='upper right', borderaxespad=0, fontsize=8)
-fig.savefig('gitlab_projects-evo_vs_forest.svg', format='svg', dpi=300)
+fig.savefig('gitlab_projects-branch-commit-6h.svg', format='svg', dpi=300)
 
 plt.show()
 
