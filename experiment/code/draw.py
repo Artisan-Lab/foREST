039b19ded7fa0d94ff345888132f5785f695d768
linjiaxian <ljx425115084@gmail.com>
2022-01-12 11:16:05 +0800

    update

--- experiment/code/draw.py
+++ experiment/code/draw.py
@@ -11,17 +11,17 @@ from datetime import datetime
 
 
 y_major_locator=MultipleLocator(600)
-forest_file = open('../data/gitlab-project/foREST_gitlab_projects.csv')  # 打开csv文件
+forest_file = open('../data/gitlab-project-branch-commit/forest.csv')  # 打开csv文件
 forestReader = csv.reader(forest_file)  # 读取csv文件
 forestData = list(forestReader)  # csv数据转换为列表
 forest_length = len(forestData)  # 得到数据行数
 
-evomaster_file = open('../data/gitlab-project/evomaster_gitlab_projects.csv')
+evomaster_file = open('../data/gitlab-project-branch-commit/evomaster.csv')
 evomaster_reader = csv.reader(evomaster_file)
 evomaster_data = list(evomaster_reader)
 evomaster_length = len(evomaster_data)
 
-restler_file = open('../data/gitlab-project/reslter.csv')
+restler_file = open('../data/gitlab-project-branch-commit/restler.csv')
 restler_reader = csv.reader(restler_file)
 restler_data = list(restler_reader)
 restler_length = len(restler_data)
@@ -37,16 +37,22 @@ y2 = list()
 x3 = list()
 y3 = list()
 for i in range(0, forest_length):  # 从第二行开始读取
+    if '0:30:' in forestData[i][0]:
+        break
     date1 = datetime.strptime(forestData[i][0], '%H:%M:%S.%f')
     x1.append(date1)  # 将第一列数据从第二行读取到最后一行赋给列表x
     y1.append(int(forestData[i][2]))
 
 for _ in range(0, evomaster_length):
+    if '0:30:' in evomaster_data[_][0]:
+        break
     date2 = datetime.strptime(evomaster_data[_][0], '%H:%M:%S.%f')
     x2.append(date2)
     y2.append(int(evomaster_data[_][2]))
 
 for _ in range(0, restler_length):
+    if '0:30:' in restler_data[_][0]:
+        break
     date3 = datetime.strptime(restler_data[_][0], '%H:%M:%S.%f')
     x3.append(date3)
     y3.append(int(restler_data[_][2]))
@@ -72,7 +78,7 @@ ax.yaxis.set_major_locator(y_major_locator)
 # plt.ylim((10000, 30000))
 plt.title('GitLab-project')
 plt.legend(bbox_to_anchor=(1, 0.11), loc='upper right', borderaxespad=0, fontsize=8)
-fig.savefig('gitlab_projects.svg', format='svg', dpi=300)
+fig.savefig('gitlab_projects-commit-branch-30min.svg', format='svg', dpi=300)
 
 plt.show()
 
